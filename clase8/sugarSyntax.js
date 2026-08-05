a747b1c63c43f47f81b89ab547e5591f4f06269b
Andrés Pata Fraile de Manterola <apfm21@gmail.com>
2026-08-05 20:32:56 -0300

    inicio clase 8

---
+++ clase8/sugarSyntax.js
@@ -0,0 +1,47 @@
+// 🔶 Ternarios 🔶
+/* 
+Un ternario es un algoritmo que combina en una sola línea un if...else con un return.
+
+condición ? siVerdadero : siFalso
+
+Si la condición dada es verdadera, hace un return del valor de siVerdadero, si es falsa, da un return del siFalso
+
+*/
+
+// 🔶 Spread Operator 🔶
+/* 
+El spread operator "..." al utilizarce separa un iterable en sus elementos. Los esparce. Es muy útil para combinar arrays y objetos.
+
+const a = ["a", "b", "c"]
+const b = [...a, "d", "e", "f"]
+
+console.log(b) // ["a", "b", "c", "d", "e", "f"]
+
+*/
+
+// 🔶 Rest parameters 🔶
+/* 
+El rest parameters utiliza el operator spread "..." al utilizarce antes de un parámetro iterable, permite que se pongan 0 o más argumentos combinandolos en un array.
+
+funcion verParametro(...parametroIterable){
+    return parametroIterable
+}
+
+console.log(verParametro(1,2,3,4)) // [1,2,3,4]
+*/
+
+
+// 🔶 Destructuring 🔶
+
+/* 
+La desestructuración permite asignar valores a elementos o propiedades de arrays y objetos para llamarlos individualmente y manipularlos.
+
+const gato = {nombre:"muichifus", anios:2}
+
+const {nombre, edad} = gato
+
+console.log(edad) // 2
+
+Es muy importante el tener en cuenta el orden de los elementos.
+
+*/
